7d374902835823c9ba673a465370f5c20986ab4d
Autonyo Sanders <tonio.sanders@gmail.com>
2019-11-20 19:30:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,8 +28,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-        array = ["am","blue","red","orange"]
-[0]
+        array = ["red","blue","am","orange"]
+        return [2]
 end
 
 def retrieve_first_element_from_array(array)
